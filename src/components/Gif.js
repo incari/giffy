66954a39151f099116022f9446b3777d2a6f7774
Martin Racana <ing.racana@gmail.com>
2021-04-07 16:18:08 +0200

    login and user sessions 2

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import {Link} from "wouter"
-import "./styles/Gif.css"
+import React from "react";
+import { Link } from "wouter";
+import Fav from "./Fav";
+import "./styles/Gif.css";
 
-function Gif({title, id, url}) {
-    return (
-        <div className="Gif">
-
-        <Link to= {`/gif/${id}`} className="Gif-link">
-            <h4>{title}</h4>
-            <img src={url} alt={title} />
-          </Link >
-        </div>
-    )
+function Gif({ title, id, url }) {
+  return (
+    <div className="Gif">
+      <div className="Gif-button">
+        <Fav id={id}/>
+      </div>
+      <Link to={`/gif/${id}`} className="Gif-link">
+        <h4>{title}</h4>
+        <img src={url} alt={title} />
+      </Link>
+    </div>
+  );
 }
-export default React.memo(Gif)
+export default React.memo(Gif);
